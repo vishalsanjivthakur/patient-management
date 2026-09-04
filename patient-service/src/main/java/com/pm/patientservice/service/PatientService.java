d4af32c128a9ae2ded2e5e2f60cce4f578678835
Vishal Thakur <vishalsanjivthakur@gmail.com>
2026-09-04 21:09:25 +0530

    Added condition to check if Email already exists, and then throw respective custom exception

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -26,6 +27,10 @@ public class PatientService {
   }
 
   public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+    if(patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+      throw new EmailAlreadyExistsException("A patient with this email already exists: " + patientRequestDTO.getEmail());
+    }
+
     Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
 
     return PatientMapper.toDTO(newPatient);
